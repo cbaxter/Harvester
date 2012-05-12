f0301e506f49e2d7da27d59527a4fe51bd4682d5
Chris Baxter <Chris.Baxter@shaw.ca>
2012-05-12 16:41:55 -0600

    Updated `package.cmd` to allow explicit setting of `Configuration`.

--- package.cmd
+++ package.cmd
@@ -15,6 +15,7 @@ echo Commit: %Commit%
 
 :: Prompt for product version
 :: --------------------------------------------------
+set /p Configuration=Configuration: %=%
 set /p MajorVersion=Major Version: %=%
 set /p MinorVersion=Minor Version: %=%
 set /p RevisionNumber=Revision Number: %=%
@@ -22,7 +23,7 @@ set /p RevisionNumber=Revision Number: %=%
 :: Run MSBuild
 :: --------------------------------------------------
 cd src
-%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild build.proj /target:Package /property:Configuration=Release /property:MajorVersion=%MajorVersion% /property:MinorVersion=%MinorVersion% /property:RevisionNumber=%RevisionNumber% /property:Commit=%Commit%
+%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild build.proj /target:Package /property:Configuration=%Configuration% /property:MajorVersion=%MajorVersion% /property:MinorVersion=%MinorVersion% /property:RevisionNumber=%RevisionNumber% /property:Commit=%Commit%
 if errorlevel 1 pause
 cd..
 
