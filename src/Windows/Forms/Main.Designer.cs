94c4fb8234c1c1903e16223b03764fe94d413e2f
Chris Baxter <Chris.Baxter@shaw.ca>
2012-05-13 10:19:16 -0600

    Stubbed in Windows project for alternate user interface.

---
+++ src/Windows/Forms/Main.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Harvester.Forms
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.systemEvents = new System.Windows.Forms.ListView();
+            this.messageId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timestamp = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.level = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.processId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.processName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.thread = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.source = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.user = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.message = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // systemEvents
+            // 
+            this.systemEvents.CausesValidation = false;
+            this.systemEvents.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.messageId,
+            this.timestamp,
+            this.level,
+            this.processId,
+            this.processName,
+            this.thread,
+            this.source,
+            this.user,
+            this.message});
+            this.systemEvents.FullRowSelect = true;
+            this.systemEvents.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.systemEvents.Location = new System.Drawing.Point(12, 85);
+            this.systemEvents.MultiSelect = false;
+            this.systemEvents.Name = "systemEvents";
+            this.systemEvents.Size = new System.Drawing.Size(984, 430);
+            this.systemEvents.TabIndex = 0;
+            this.systemEvents.UseCompatibleStateImageBehavior = false;
+            this.systemEvents.View = System.Windows.Forms.View.Details;
+            // 
+            // messageId
+            // 
+            this.messageId.Text = "Id";
+            // 
+            // timestamp
+            // 
+            this.timestamp.Text = "Timestamp";
+            this.timestamp.Width = 100;
+            // 
+            // level
+            // 
+            this.level.Text = "Level";
+            this.level.Width = 100;
+            // 
+            // processId
+            // 
+            this.processId.Text = "PID";
+            this.processId.Width = 100;
+            // 
+            // processName
+            // 
+            this.processName.Text = "Process Name";
+            this.processName.Width = 100;
+            // 
+            // thread
+            // 
+            this.thread.Text = "Thread";
+            this.thread.Width = 100;
+            // 
+            // source
+            // 
+            this.source.Text = "Source";
+            this.source.Width = 100;
+            // 
+            // user
+            // 
+            this.user.Text = "User";
+            this.user.Width = 100;
+            // 
+            // message
+            // 
+            this.message.Text = "Message";
+            this.message.Width = 100;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 730);
+            this.Controls.Add(this.systemEvents);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView systemEvents;
+        private System.Windows.Forms.ColumnHeader messageId;
+        private System.Windows.Forms.ColumnHeader timestamp;
+        private System.Windows.Forms.ColumnHeader level;
+        private System.Windows.Forms.ColumnHeader processId;
+        private System.Windows.Forms.ColumnHeader processName;
+        private System.Windows.Forms.ColumnHeader thread;
+        private System.Windows.Forms.ColumnHeader source;
+        private System.Windows.Forms.ColumnHeader user;
+        private System.Windows.Forms.ColumnHeader message;
+    }
+}
---
+++ src/Windows/Forms/Main.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+using Harvester.Core;
+using Harvester.Core.Messaging;
+
+/* Copyright (c) 2012 CBaxter
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ * IN THE SOFTWARE. 
+ */
+
+namespace Harvester.Forms
+{
+    public partial class Main : Form, IRenderEvents
+    {
+        private readonly SystemMonitor systemMonitor;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            systemMonitor = new SystemMonitor(this);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            //systemMonitor.Dispose();
+        }
+
+        public void Render(SystemEvent e)
+        {
+            //TODO: enqueue messages for display, setup timer to update list view every 100ms
+
+            var item = CreateListViewItem(e);
+
+            if (systemEvents.InvokeRequired)
+            {
+                systemEvents.Invoke((Action) (() => systemEvents.Items.Add(item)));
+            }
+            else
+            {
+                systemEvents.Items.Add(item);
+            }
+        }
+
+        private ListViewItem CreateListViewItem(SystemEvent e)
+        {
+            return new ListViewItem(new[]
+                       {
+                           e.MessageId.ToString(CultureInfo.InvariantCulture),
+                           e.Timestamp.ToString("yyyy-MM-dd HH:mm:ss,fff"),
+                           e.Level.ToString(),
+                           e.ProcessId.ToString(CultureInfo.InvariantCulture),
+                           e.ProcessName,
+                           e.Thread,
+                           e.Source,
+                           e.Username,
+                           e.Message
+                       }) {Tag = e};
+        }
+    }
+}
