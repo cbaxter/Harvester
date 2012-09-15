b3c18e386644c3159d765faa778775cbe968a477
Chris Baxter <Chris.Baxter@shaw.ca>
2012-09-15 08:44:43 -0600

    Added NuGet pack to build.proj.

--- src/Core.Tests/Properties/AssemblyInfo.cs
+++ src/Core.Tests/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Harvester.Core")]
+[assembly: AssemblyTitle("Harvester.Core.Tests")]
 [assembly: AssemblyDescription("Harvester - Core (Unit Tests)")]
--- src/NLog/Properties/AssemblyInfo.cs
+++ src/NLog/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Harvester - NLog Target")]
-[assembly: AssemblyDescription("Harvester - NLog Target for .NET 4 Debugger")]
+[assembly: AssemblyTitle("Harvester.Integration.NLog")]
+[assembly: AssemblyDescription("Harvester - NLog Target")]
